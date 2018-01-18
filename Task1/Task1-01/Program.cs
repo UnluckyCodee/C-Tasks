ae67de8ce3db7aca16c00e437e88f4acdbbd000d
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-18 12:09:03 +0400

    added task 1.01

---
+++ Task1/Task1-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine($"Площадь прямоугольника: {a * b}");   
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
