84f6f1df57fec90f12b30247d0de838cb3fb05d4
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-18 12:58:01 +0400

    added task1-03

---
+++ Task1/Task1-03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int n = 1; n <= N; n++)
+            {
+                Console.Write(new string(' ', N - n));
+                Console.WriteLine(new string('*', 2*n-1));
+            }
+        }
+    }
+}
