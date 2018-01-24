3e25de7bc623ffec517f454f3a8f2995bd49d28d
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-24 10:46:15 +0400

    added task1-09

---
+++ Task1/Task1_09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            double[] Array = new double[n];
+
+            Console.WriteLine("Ввод Элемента:");
+            { 
+            for (int i = 0; i < n; i++)
+            
+                 Array[i] = double.Parse(Console.ReadLine());
+                double Sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (Array[i] > 0)
+                    {
+                        Sum += Array[i];
+                    }
+                
+        }
+         Console.WriteLine($"Сумма неотрицательных элементов массива = { Sum }");
+         Console.ReadKey();
+             }
+        }
+    }
+}
