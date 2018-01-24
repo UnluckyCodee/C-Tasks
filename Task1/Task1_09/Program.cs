fb79fa16fcd1c9de497f6b89c7ab5a14b49eb1a7
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-24 11:58:09 +0400

    refactored task1-09

--- Task1/Task1_09/Program.cs
+++ Task1/Task1_09/Program.cs
@@ -8,28 +8,46 @@ namespace Task1_09
 {
     class Program
     {
+        private static Random rand = new Random();
+
+        public static int[] GenerateArray()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-50, 50);
+            }
+            return arr;
+        }
+        private static void WriteArray(int[] arr)
+        {
+            Console.WriteLine("Элементы массива:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+            }
+        }
+        private static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
-            const int n = 5;
-            double[] Array = new double[n];
-
-            Console.WriteLine("Ввод Элемента:");
-            { 
-            for (int i = 0; i < n; i++)
-            
-                 Array[i] = double.Parse(Console.ReadLine());
-                double Sum = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    if (Array[i] > 0)
-                    {
-                        Sum += Array[i];
-                    }
-                
-        }
-         Console.WriteLine($"Сумма неотрицательных элементов массива = { Sum }");
-         Console.ReadKey();
-             }
+            int[] arr = GenerateArray();
+            WriteArray(arr);
+            Sum(arr);
+            Console.WriteLine($"Сумма неотрицательных элементов массива: {Sum(arr)}");
+            Console.ReadKey();
         }
     }
 }
