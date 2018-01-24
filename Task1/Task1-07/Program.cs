9a0e5d3b55bf623b733133f4f9b4641ecb072f45
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-24 10:45:42 +0400

    added task1-07

---
+++ Task1/Task1-07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_07
+{
+    class Program
+    {
+        private static Random rand = new Random();
+
+        public static int[] GenerateArray(int n)
+        {
+            n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(100);
+            }
+            return arr;
+        }
+
+        private static void WriteArray(int[] arr)
+        {
+            Console.WriteLine("Элементы массива:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = GenerateArray(10);
+            WriteArray(arr);           
+            Console.ReadLine();
+        }
+    }
+}
