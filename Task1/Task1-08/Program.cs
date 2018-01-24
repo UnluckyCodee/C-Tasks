5250cc266762e93ac5cdb76970bbef99f351e0d3
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-24 10:46:02 +0400

    added task1-08

---
+++ Task1/Task1-08/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввод Размерности:");
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            int[,,] arr;
+            arr = new int[x, y, z];
+            double[,,] Array = new double[x, y, z];
+            {
+                Console.WriteLine("Ввод Элемента:");
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        for (int b = 0; b < z; b++)
+                        {
+                            Console.Write("Array[" + i + "," + j + "," + b + "]: ");
+                            Array[i, j, b] = int.Parse(Console.ReadLine());
+                        }
+                    }
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        for (int b = 0; b < z; b++)
+                        {
+                            if (Array[i, j, b] > 0)
+                            {
+                                Array[i, j, b] = 0;
+                            }
+                            Console.WriteLine($"Array: {Array[i, j, b]}");
+
+                        }
+
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
