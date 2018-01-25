04373eab1bc5118b9238321030dfcc0f2bf5815b
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-01-25 11:54:53 +0400

    added task1-12

---
+++ Task1/Task1-12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string firstString = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string secondString = Console.ReadLine();
+
+            Console.WriteLine($"Результирующая строка: {Duplicate(firstString, secondString)}");
+        }
+        private static string Duplicate(string firstString, string secondString)
+        {
+            var sb = new StringBuilder(2 * firstString.Length);
+            {
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    if (!secondString.Contains(firstString[i]))
+                    {
+                        sb.Append(firstString[i]);
+                    }
+                    else
+                    {
+                        sb.Append(firstString[i]);
+                        sb.Append(firstString[i]);
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
