11f44466d7ec649c2c4754d8d0b16ea2d32b1896
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-02-01 11:18:53 +0400

    added task1-addition-1

---
+++ Task1/Task1-addition-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_addition_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string firstString = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string secondString = Console.ReadLine();
+
+            firstString = firstString.Replace(secondString, "$");
+            
+            Console.WriteLine($"Результирующая строка: { (firstString)}");
+        }
+
+    }
+}
