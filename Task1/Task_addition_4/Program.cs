a0753d51aea44d414003ed2e0d17412f400058f6
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-02-01 11:20:28 +0400

    added task1_addition_4

---
+++ Task1/Task_addition_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_addition_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string words = Console.ReadLine();
+            Console.WriteLine($"Обработанная строка:  {Rep(words)}");
+        }
+
+        private static string Rep(string words)
+        {
+            char[] seps = { ' ' };
+            string[] parts = words.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                words = String.Join(" ", parts);
+            }
+            return words;
+        }
+    }
+}
