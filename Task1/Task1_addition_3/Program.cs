4d8c12360a6d9d7639ba813806d2331cb1d561cc
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-02-01 11:20:19 +0400

    added task1_addition_3

---
+++ Task1/Task1_addition_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_addition_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string words = Console.ReadLine();
+            if (IsPalindrom(words))
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+
+        private static bool IsPalindrom(string words)
+        {
+            int i = 0;
+            for (i = 0; i < words.Length / 2; i++)
+            {
+                if (words[i] != words[words.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
