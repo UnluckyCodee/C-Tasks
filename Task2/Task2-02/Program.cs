fdacd94d9cfb43003e5ea881e6b89e3f16f78e3b
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-02-01 12:00:47 +0400

    added task2-02

---
+++ Task2/Task2-02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle_1 = new Triangle();
+            Triangle triangle_2 = new Triangle(5, 5, 5);
+            Console.WriteLine($"Треугольник со сторонами ({triangle_1.A},{triangle_1.B},{triangle_1.C}) Имеет площадь ({triangle_1.Area}) И периметр ({triangle_1.Perimeter}) ");
+            Console.WriteLine($"Треугольник со сторонами ({triangle_2.A},{triangle_2.B},{triangle_2.C}) Имеет площадь ({triangle_2.Area}) И периметр ({triangle_2.Perimeter}) ");
+
+        }
+    }
+}
---
+++ Task2/Task2-02/Triangle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_02
+{
+    class Triangle
+    {
+        private double a, b, c;
+
+        public double A
+        {
+            get
+            {
+                return this.a;
+            }
+          set
+            {
+                CheckExceptions(value, this.b, this.c);
+                this.a = value;
+            }
+        }
+
+        public double B
+        {
+            get
+            {
+                return this.b;
+            }
+            set
+            {
+                CheckExceptions(this.a, value, this.c);
+                this.b = value;
+            }
+        }
+
+        public double C
+        {
+            get
+            {
+                return this.c;
+            }
+            set
+            {
+                CheckExceptions(this.a, this.b, value);
+                this.c = value;
+            }
+        }
+
+        private void CheckExceptions(double a, double b, double c)
+        {
+            if (a + b < c || a + c < b || b + c < a)
+            {
+                throw new ArgumentException(" Загуглить правила создания треугольникуа лол ");
+            }
+        }
+
+        public Triangle()
+        {
+            this.a = 1;
+            this.b = 1;
+            this.c = 1;
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            CheckExceptions(a, b, c);
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+
+        public double Perimeter => this.a + this.b + this.c;
+        public double Area
+        {
+            get
+            {
+                double P = this.Perimeter / 2;
+                return Math.Sqrt(P * ( P - this.a) * P * ( P - this.b) * P * ( P - this.c)) ;
+            }
+        }
+    }
+}
