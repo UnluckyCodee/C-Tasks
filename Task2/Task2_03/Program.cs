d59738ee4b34db825cc52dd31afe84125af4b651
UnluckyCodee <35555789+UnluckyCodee@users.noreply.github.com>
2018-02-02 11:01:02 +0400

    added task2-03

---
+++ Task2/Task2_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Ivanov", "Ivan", "Ivanovich", new DateTime(1995, 8, 19));
+            Console.WriteLine($"Иия {user.Name} Фамилия {user.Surname} Отчество {user.Patronymic} Дата рождения ({user.Birthday.ToString("yyyy/MM/dd")})");
+            user.Birthday = new DateTime(1119, 7, 13);
+        }
+    }
+}
---
+++ Task2/Task2_03/User.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_03
+{
+    class User
+    {
+        private string name, surname, patronymic;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                CheckNotNull(value, this.surname, this.patronymic);
+                this.name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return this.surname;
+            }
+            set
+            {
+                CheckNotNull(this.name, value, this.patronymic);
+                this.surname = value;
+            }
+        }
+
+        public string Patronymic
+        {
+            get
+            {
+                return this.patronymic;
+            }
+            set
+            {
+                CheckNotNull(this.name, this.surname, value);
+                this.patronymic = value;
+            }
+        }
+
+        private void CheckNotNull(string name, string surname, string patronymic)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname) || string.IsNullOrEmpty(patronymic))
+            {
+                throw new ArgumentException("ФИО не должно содержать пустых значений");
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get
+            {
+                return this.birthday;
+            }
+            set
+            {
+                int age = CalculateDifference(value, DateTime.Now);
+                if (age > 150)
+                {
+                    throw new ArgumentException("Данные введены некорректно, проверьте год рождения");
+                }
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Дата рождения не может быть больше сегодняшней");
+                }
+                this.birthday = value;
+            }
+        }
+
+        private int CalculateDifference(DateTime start, DateTime end)
+        {
+            int age = end.Year - start.Year;
+            if (start > DateTime.Now.AddYears(-age));
+            {
+                age--;
+            }
+            return age;
+        }
+
+        public int Age => CalculateDifference(this.birthday, DateTime.Now);
+
+        public User(string name, string surname, string patronymic, DateTime birthday)
+        {
+            CheckNotNull(name, surname, patronymic);
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+            this.Birthday = birthday;
+        }
+    }
+}
